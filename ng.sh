9a63832beb260d10096a98022f8ca55bf95e2e64
mandexjunior2000 <91593573+mandexjunior2000@users.noreply.github.com>
2023-03-02 22:40:06 +0600

    Update ng.sh

--- ng.sh
+++ ng.sh
@@ -2,7 +2,7 @@ rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
 echo "======================="
 echo "Downloading ngrok"
 echo "======================="
-wget -O ngrok.zip https://github.com/mandexjunior2000/gconsole-kali/blob/main/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok.zip > /dev/null 2>&1
 read -p "Paste Ngrok Authtoken: " CRP
 ./ngrok authtoken $CRP 
